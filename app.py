6177659a3864cd0e4538cbc9748f42e2ec1e058f
Eric Persiani <eric.persiani@gmail.com>
2020-08-08 12:42:54 -0500

    Fix not importing db when running the app.

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'eric'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
