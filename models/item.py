d143c9ad92a6b1066f9eebed979983f54d000f2e
Eric Persiani <eric.persiani@gmail.com>
2020-08-10 14:07:03 -0500

    Fix error in the item.py file preventing the app from running

--- models/item.py
+++ models/item.py
@@ -17,7 +17,7 @@ class ItemModel(db.Model):
 
     def json(self):
         return {
-            'id'" self.id,
+            'id': self.id,
             'name': self.name,
             'price': self.price,
             'store_id': self.store_id
