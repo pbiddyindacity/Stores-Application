11c3312dafecab282feee6f0b9bf1115292e9ec5
Eric Persiani <eric.persiani@gmail.com>
2020-08-11 12:08:04 -0500

    First pass at incorporating JWT-Extended for token refreshing

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import os
 
 from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from datetime import timedelta
-from security import authenticate, identity as identity_function
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -15,22 +14,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_SECRET_KEY'] = 'eric'
 app.secret_key = 'eric'
 api = Api(app)
 
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
-jwt = JWT(app, authenticate, identity_function)
-
-
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity):
-    return jsonify({
-        'access_token': access_token.decode('utf-8'),
-        'user_id': identity.id
-    })
-
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -38,7 +29,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
-
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -1,14 +1,18 @@
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
 from flask_restful import Resource, reqparse
 
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username', type=str, required=True, help="This field cannot be blank.")
+_user_parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
+
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username', type=str, required=True, help="This field cannot be blank.")
-    parser.add_argument('password', type=str, required=True, help="This field cannot be blank.")
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
+        
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
@@ -33,3 +37,22 @@ class User(Resource):
             return {'message': 'User not found'}, 404
         user.delete_from_db()
         return {'message': 'User deleted'}, 200
+
+
+class UserLogin(Resource):
+
+    def post(cls):
+        data = _user_parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password, data['password']):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+
+        return {'message': 'Invalid credentials'}, 401
+        
+        
