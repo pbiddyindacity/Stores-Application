1049961ca1ea0b0c88c508dea5340fa32a6f6462
Eric Persiani <eric.persiani@gmail.com>
2020-08-11 12:26:28 -0500

    Fix import for flask-jwt-extended

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,6 @@
 from models.item import ItemModel
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 
 
 class Item(Resource):
