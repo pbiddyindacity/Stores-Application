4dcc72fef3ad3d2cd1473f6438e22b20659d8573
Eric Persiani <eric.persiani@gmail.com>
2020-08-11 17:35:28 -0500

    Add custom JWT callbacks and blacklist functionality

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,11 @@
 from models.item import ItemModel
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_claims,
+    jwt_optional,
+    get_jwt_identity,
+    fresh_jwt_required)
 
 
 class Item(Resource):
@@ -24,6 +29,7 @@ class Item(Resource):
             return item.json()
         return {"message": "Item not found"}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already "
